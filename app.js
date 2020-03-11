2f654186f5e27401d66cc19f38a1a77c0d7a5523
Mel Garza <melgarza3016@gmail.com>
2020-03-10 19:51:39 -0600

    connected server

---
+++ app.js
@@ -0,0 +1,26 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const consoleTable = require("console.table");
+const colors = require("colors");
+colors.enable();
+
+
+let roles;
+let managers;
+
+//Create Connection
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Iveg0tal0velybunch0fc0c0nuts",
+    database: "trackerDB"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+
+    srcByRole();
+    initApplication();
+});
